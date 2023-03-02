692cf90d106f3c237e216c0e51085a22a7cd259e
YurTorosyan <torosyan.yuri17@gmail.com>
2023-03-03 01:20:18 +0400

    Header Complete!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
+import Header from './Header';
+
+export default function App() {
   return (
     <div>
+      <Header />
     </div>
   );
 }
 
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div>
+            <header className="header">
+                <h1 className='header__title'>Random Hex Color for CSS</h1>
+                <p className="header__info">This Page created by <a href="https://github.com/YurTorosyan" target="_blank">Yuri Torosyan</a> with the help of <a href="https://reactjs.org/" target="_blank">React</a></p>
+            </header>
+
+        </div>
+    );
+}
