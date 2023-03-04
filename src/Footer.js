9627f316684195a2c660814d58ee11665609d242
YurTorosyan <torosyan.yuri17@gmail.com>
2023-03-04 13:21:17 +0400

    small bugs fixed and adaptive Completed!

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,11 @@ export default function Footer() {
     return (
         <footer className='footer'>
             <div className="container">
-                <p className="footer__info">This Page created by <a href="https://github.com/YurTorosyan" target="_blank">Yuri Torosyan</a> with the help of <a href="https://reactjs.org/" target="_blank">React</a></p>
-                <p className="footer__copyright">&copy; Copyright 2023</p>
-                <p className="footer__source">RandomHex Github <a href="https://github.com/YurTorosyan/random_color_page" target="_blank">Source</a> page</p>
+                <div className="footer__wrap">
+                    <p className="footer__source">RandomHex Github <a href="https://github.com/YurTorosyan/random_color_page" target="_blank">Source</a> page</p>
+                    <p className="footer__info">This Page created by <a href="https://github.com/YurTorosyan" target="_blank">Yuri Torosyan</a> with the help of <a href="https://reactjs.org/" target="_blank">React</a></p>
+                    <p className="footer__copyright">&copy; Copyright 2023</p>
+                </div>
             </div>
         </footer>
     )
